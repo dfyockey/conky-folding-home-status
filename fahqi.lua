d6a90426a2ca9914c5da578377b010a610970ae2
dfyockey <git@diffengine.net>
2021-04-28 13:47:39 -0400

    Cleaned up comments

--- fahqi.lua
+++ fahqi.lua
@@ -50,10 +50,8 @@ Note — To Add Other Data Values:
   at https://github.com/FoldingAtHome/fah-control/wiki/3rd-party-FAHClient-API.
 --]]
 keys = {"project", "percentdone", "state", "eta"}
---info = {"0", "0.0", "LOADING", "00:00:00"}
 info = {}
 iter = 1
---qiSlots = {}
 
 -- Number of conky iterations between each reload of queue info;
 -- use to reduce conky impact on cpu time.
@@ -68,27 +66,13 @@ function conky_load_fah_queue_info()
     end
     
     if iter == throttle then 
-
-		-- cmd = '{ echo "queue-info"; sleep 1; } | telnet localhost 36330 | grep \'"id": "00"\''
-		
-		-- This shorter command was suggested by the post at
+		-- The following command, obviating need for use of telnet, was suggested by the post at
 		-- https://foldingforum.org/viewtopic.php?f=88&t=25050&p=249988&hilit=conky#p250414
-		--cmd = 'FAHClient --send-command queue-info | grep \'"id": "01"\''
 		cmd = 'FAHClient --send-command queue-info'
 		
 		local f = assert(io.popen(cmd, 'r'))
 		local qi = assert(f:read('a'))
 		f:close()
-
---[[
-		for qiSlot in string.gmatch(qi,'({[^}]*})') do
-			idSlot = string.match(qiSlot,'"id": "(%d%d)"')
-			qiSlots[idSlot] = qiSlot
-		end
-		
-		idSlot = "01"
-		qiSlot = qiSlots[idSlot]
-]]	
 		
 		for qiSlot in string.gmatch(qi,'({[^}]*})') do
 			idSlot = string.match(qiSlot,'"id": "(%d%d)"')
@@ -105,10 +89,6 @@ function conky_load_fah_queue_info()
 					value = string.match(keyvalue, ': "[^"]+"')
 								
 					if keys[k] == "eta" then
-						-- Time Info
-						
-						-- Time info data are time values or labels
-
 						-- strip off garbage
 						value = string.gsub(value, ': ', '')
 						value = string.gsub(value, '"', '')
@@ -143,8 +123,9 @@ function conky_load_fah_queue_info()
 
 							else
 								-- % in string.match is an escape character, so '%.' is a literal dot
+								--
 								-- % in each string.format begins a conversion specification, which ends with a converison character (f or d)
-								--     (The format string follows the same rules as the ISO C function sprintf)
+								---- The string.format format string follows the same rules as the ISO C sprintf function
 								if string.match(datum, '%.') then
 									tt = string.format('%1.2f', datum)
 								else
@@ -158,16 +139,9 @@ function conky_load_fah_queue_info()
 						value = ''
 						if dd ~= "00" then
 							-- set symbol for "day"
-							--value = dd .. "${font FreeSerif:size=11}${color}☌${color grey}${font}"
 							value = dd .. "d"
 						else
-							--if hh ~= "00" then value = value..hh end
 							value = string.format( '%2.2fh', ( ( (hh*3600) + (mm*60) + ss ) / 3600) )
-							--value = value..'h'
-						
-							-- if hh ~= "00" then value = value .. hh .. 'h:' end
-							-- if dd == "00" then value = value .. mm .. 'm' end
-							-- if hh == "00" and dd == "00" then value = value .. ':' .. ss .. 's' end
 						end
 					
 					elseif keys[k] == "state" then
